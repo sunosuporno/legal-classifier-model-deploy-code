4c6dc708542088734204256d6901474e10609515
sunosuporno <sarkarsuporno36@gmail.com>
2023-12-08 02:35:22 +0530

    added more info on clicking classify

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-'''
-
 import streamlit as st
 import pickle
 import string
@@ -9,105 +7,10 @@ from nltk.stem.porter import PorterStemmer
 import requests
 from bs4 import BeautifulSoup
 import os
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import Counter  # Import Counter for word frequency
 
-ps = PorterStemmer()
-
-def transform_text(text):
-    text = text.lower()
-    text = nltk.word_tokenize(text)
-
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
-
-def fetch_text_from_url(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        paragraphs = soup.find_all('p')
-        text = ' '.join([paragraph.get_text() for paragraph in paragraphs])
-        return text
-    except Exception as e:
-        return str(e)
-
-# Load the Multinomial Naive Bayes model
-model_path = 'model.pkl'
-with open(model_path, 'rb') as model_file:
-    model = pickle.load(model_file)
-
-# Load the TF-IDF vectorizer
-vectorizer_path = 'vectorizer.pkl'
-with open(vectorizer_path, 'rb') as vectorizer_file:
-    tfidf = pickle.load(vectorizer_file)
-
-# Load training data
-training_data_path = 'training_data.csv'
-train_df = pd.read_csv(training_data_path)
-X_train = tfidf.transform(train_df['text']).toarray()
-y_train = train_df['target'].values
-
-# Additional print statements for debugging
-print("Loaded Model Parameters:", model.get_params())
-
-st.title("Legal Document Classifier")
-
-input_url = st.text_input("Enter the link")
-
-if st.button('Classify'):
-    if input_url:
-        # Fetch text from the URL
-        fetched_text = fetch_text_from_url(input_url)
-
-        # 1. preprocess
-        transformed_sms = transform_text(fetched_text)
-        # 2. vectorize
-        vector_input = tfidf.transform([transformed_sms])
-        # Before prediction
-        st.write("Transformed SMS:", transformed_sms)
-        st.write("Vector Input:", vector_input)
-        # 3. predict
-        # Fit the model with appropriate data before prediction
-        model.fit(X_train, y_train)
-        result = model.predict(vector_input)[0]
-        # 4. Display
-        if result == 1:
-            st.header("Not Cybercrime")
-        else:
-            st.header("Cybercrime")
-    else:
-        st.warning("Please enter a valid URL.")
-'''
-
-import streamlit as st
-import pickle
-import string
-from nltk.corpus import stopwords
-import nltk
-from nltk.stem.porter import PorterStemmer
-import requests
-from bs4 import BeautifulSoup
-import os
 print(os.path.isfile('model.pkl'))
 
-
 ps = PorterStemmer()
 
 def transform_text(text):
@@ -144,13 +47,11 @@ def fetch_text_from_url(url):
     except Exception as e:
         return str(e)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 print(type(model))
 print("Model Parameters:", model.get_params())
 
-
-
 st.title("Legal Document Classifier")
 
 input_url = st.text_input("Enter the link")
@@ -166,10 +67,44 @@ if st.button('Classify'):
         vector_input = tfidf.transform([transformed_sms])
         # 3. predict
         result = model.predict(vector_input)[0]
-        # 4. Display
+
+        # 4. Display result
         if result == 1:
             st.header("Not Cybercrime")
         else:
             st.header("Cybercrime")
+
+        # Additional features
+        # Word count
+        word_count = len(transformed_sms.split())
+        # Word frequency
+        words_frequency = Counter(transformed_sms.split())
+        # Check if numbers 66 appear
+
+        # Display outputs side by side
+        col1, col2 = st.columns(2)
+
+        # Column 1
+        with col1:
+            st.write(f"Word Count: {word_count}")
+            st.write("Word Frequency:")
+            for word, count in words_frequency.most_common(5):  # Display top 5 words
+                st.write(f"{word}: {count}")
+
+        # Column 2
+        with col2:
+            occurrences_66 = words_frequency.get('66', 0)
+            st.write(f"Occurrences of Section '66': {occurrences_66}")
+            occurrences_67 = words_frequency.get('67', 0)
+            st.write(f"Occurrences of Section '67': {occurrences_67}")
+            occurrences_65 = words_frequency.get('65', 0)
+            st.write(f"Occurrences of Section '65': {occurrences_65}")
+
+
     else:
         st.warning("Please enter a valid URL.")
+
+
+
+# https://indiankanoon.org/doc/56699948/
+# https://indiankanoon.org/doc/1030160/
